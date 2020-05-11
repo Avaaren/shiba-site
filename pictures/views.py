24848096e546e658435b2bb2f319fddb87862c5e
Avaaren <avaaren@mail.ru>
2020-05-11 16:02:35 +0300

    Added view, displaying pictures

--- pictures/views.py
+++ pictures/views.py
@@ -1,1 +1,16 @@
+from django.views.generic import TemplateView
+
+import requests
+
+class GetShibaImage(TemplateView):
+    template_name = 'pictures/shibe.html'
+
+    def get_context_data(self, **kwargs):
+        response = requests.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
+        image_url = response.json()[0]
+        data = super(GetShibaImage, self).get_context_data(**kwargs)
+        data['image_url'] = image_url
+
+        return data
+
 
