98fc212ec2ece1b649479517b45183799da17e33
Avaaren <avaaren@mail.ru>
2020-05-12 12:36:09 +0300

    Added file for functions. Added saving file function

---
+++ pictures/utils.py
@@ -0,0 +1,24 @@
+import os
+
+def save_file(filename):
+    try:
+        # Get home directory
+        home = os.path.expanduser('~')
+        # If Downloads folder is in home directory
+        if os.path.exists(os.path.join(home, 'Downloads')):
+            # Set path to file home/downloads/filename
+            filename = os.path.join(os.path.join(home, 'Downloads'), filename)
+        # Elif Загрузки folder in home directory
+        elif os.path.exists(os.path.join(home, 'Загрузки')):
+            # Set path to file home/загрузки/filename
+            filename = os.path.join(os.path.join(home, 'Загрузки'), filename)
+        else:
+            # Set path to file home/filename
+            filename = os.path.join(home, filename)
+        # Open file in download folder
+        with open(filename, 'wb') as f:
+            # And write binary file from response to it
+            f.write(response.content)
+        return True
+    except:
+        return False
