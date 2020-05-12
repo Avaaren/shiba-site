799029f618e89f398824e8d8f389f804205be81a
Avaaren <avaaren@mail.ru>
2020-05-12 17:11:28 +0300

    Added url for file download

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.GetShibaImage.as_view(), name = 'main_page'),
     path('download/', views.DownloadImage.as_view(), name = 'download'),
+    path('download/<str:filename>', views.save_file, name = 'save')
 ]
