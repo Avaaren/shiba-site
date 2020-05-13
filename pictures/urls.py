fa208dfb2c189a9694610feef5fda8e68beb9b9f
Avaaren <avaaren@mail.ru>
2020-05-13 12:16:02 +0300

    Renamed urls

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GetShibaImage.as_view(), name = 'main_page'),
-    path('download/', views.DownloadImage.as_view(), name = 'download'),
-    path('download/<str:filename>', views.save_file, name = 'save')
+    path('', views.GetShibaImage.as_view(), name='main_page'),
+    
+    path('download/',
+         views.DownloadClickHandle.as_view(),
+         name='download_click'),
+
+    path('download/<str:filename>',
+         views.DownloadFileView.as_view(),
+         name='save_file')
 ]
