a6cd8c67918c1bcc29913ef40b9000d2b6765231
Avaaren <avaaren@mail.ru>
2020-05-13 13:49:25 +0300

    Added session cookie age

--- shibe_site/settings.py
+++ shibe_site/settings.py
@@ -120,3 +120,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SESSION_COOKIE_AGE = 52560000
