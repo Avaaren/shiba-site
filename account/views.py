a2542d9d1bb18ce89cb6710d53c571cfcc2221cc
Avaaren <avaaren@mail.ru>
2020-05-13 22:08:41 +0300

    Added CBV for signup

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegistrationForm
+
+class SignupView(CreateView):
+
+    template_name = 'account/registration.html'
+    form_class = RegistrationForm
+    # success_url = reverse_lazy('account:signup-done')
+
+    def form_valid(self, form):
+
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['repeat_password'])
+        new_user.save()
+
+        return super().form_valid(form)
+
+
+    
