e8e117a95580a6bf8d1eb62d2ea553572656a388
Avaaren <avaaren@mail.ru>
2020-05-13 22:07:40 +0300

    Added urls for auth app

--- shibe_site/urls.py
+++ shibe_site/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include('pictures.urls')),
-    path('account/', include('accounts.urls')),
+    path('account/', include('account.urls')),
 ]
