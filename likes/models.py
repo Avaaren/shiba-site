8583829553d8c135b70c9f84a103e3ddebbce8f5
Avaaren <avaaren@mail.ru>
2020-05-14 11:50:28 +0300

    Added likes model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Liked(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image_href = models.CharField('Ссылка на страницу', max_length=300, db_index=True)
+
+    def __str__(self):
+        return f'{user}-{image_href}'
