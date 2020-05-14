2b5def4e282ffb397238e2b7369c556bab6c2036
Avaaren <avaaren@mail.ru>
2020-05-14 12:52:50 +0300

    Registered liked model on admin site

--- likes/admin.py
+++ likes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Liked
+
+
+@admin.register(Liked)
+class LikedAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'image_href']
 # Register your models here.
