b9bef0f5fbfecf55981c7c98a6f2983250462864
Avaaren <avaaren@mail.ru>
2020-05-14 12:53:26 +0300

    Added like dislike ajax handling

--- likes/views.py
+++ likes/views.py
@@ -1,7 +1,25 @@
 from django.views.generic import View
+from django.http import JsonResponse
+
+from .models import Liked
 
 
 class ToggleLikeView(View):
 
     def post(self, request):
-        pass
+        json_response = {}
+
+        image_url = request.POST.get('image_url', None)
+        if image_url:
+            image,created = Liked.objects.get_or_create(user=self.request.user, image_href=image_url)
+            if not created:
+                image.delete()
+                json_response['message'] = 'unliked'
+            else:
+                json_response['message'] = 'liked'
+        return JsonResponse(json_response)
+
+            
+
+
+
